fe3efb9ba62934714e2d453a40bdaeda6a33f1db
davidbacter01 <davidbacter01@gmail.com>
2020-09-15 12:01:50 +0300

    adds more base files and base template

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,19 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
-# Create an instance of the Flask class that is the WSGI application.
-# The first argument is the name of the application module or package,
-# typically __name__ when using a single module.
 app = Flask(__name__)
 
-# Flask route decorators map / and /hello to the hello function.
-# To add other resources, create functions that generate the page contents
-# and add decorators to define the appropriate resource locators for them.
+blogs = []
 
 @app.route('/')
-@app.route('/hello')
-def hello():
+@app.route('/index')
+def hello(name = None):
     # Render the page
-    return "Hello Python!"
+    return render_template('index.html', name = name)
 
 if __name__ == '__main__':
     # Run the app server on localhost:4449
