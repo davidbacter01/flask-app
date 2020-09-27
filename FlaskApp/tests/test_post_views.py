c78bd5377f33ca5db42e76bc7aefbe106ef8f53e
davidbacter01 <davidbacter01@gmail.com>
2020-09-28 00:28:54 +0300

    test new_post blueprint

--- FlaskApp/tests/test_post_views.py
+++ FlaskApp/tests/test_post_views.py
@@ -15,3 +15,24 @@ def client():
 def test_index_route(client):
     response = client.get('/')
     assert b'Red flowers' in response.data
+
+
+def test_new_post_get_route(client):
+    response = client.get('/new')
+    assert b'Title' in response.data
+    assert b'Owner' in response.data
+    assert b'Content' in response.data
+
+
+def test_new_post_post_route(client):
+    response = client.post('/new', data=dict(
+        title= 'test_title',
+        contents= 'test_contents',
+        owner= 'test_owner'
+        ), follow_redirects=True)
+
+    print(response.data)
+    assert b'test_title' in response.data
+    assert b'test_owner' in response.data
+
+
