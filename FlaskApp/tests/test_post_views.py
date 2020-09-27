2224557ff2afcb0c2089f3e124ab73648fa723ed
davidbacter01 <davidbacter01@gmail.com>
2020-09-28 01:06:26 +0300

    test view and edit views

--- FlaskApp/tests/test_post_views.py
+++ FlaskApp/tests/test_post_views.py
@@ -31,8 +31,22 @@ def test_new_post_post_route(client):
         owner= 'test_owner'
         ), follow_redirects=True)
 
-    print(response.data)
     assert b'test_title' in response.data
     assert b'test_owner' in response.data
 
+
+def test_view_post(client):
+    assert b'text about red flowers' in client.get('/view/1').data
+    assert b'text about yellow flowers' in client.get('/view/2').data
+    assert b'text about blue flowers' in client.get('/view/3').data
+
+
+def test_edit_post(client):
+    response = client.post('/edit/1', data=dict(
+        title='no flowers',
+        contents='yes flowers'
+        ), follow_redirects=True)
+    assert b'no flowers' in response.data
+    assert b'yes flowers' in response.data
+    assert b'Red flowers' not in response.data
 
