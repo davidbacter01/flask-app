24859425ee4f75da82efc84b1b8a701cf5b31f75
davidbacter01 <davidbacter01@gmail.com>
2020-10-09 15:54:46 +0300

    create UserRepositoryInterface

---
+++ FlaskApp/repository/user_repository_interface.py
@@ -0,0 +1,48 @@
+import abc
+from models.user import User
+
+
+class UserRepositoryInterface(abc.ABC):
+
+    """interface for user repository"""
+
+    @classmethod
+    def __subclasshook__(cls, subclass):
+        return (hasattr(subclass, 'get_by_id') and
+                callable(subclass.get_by_id) and
+                hasattr(subclass, 'get_all') and
+                callable(subclass.get_all) and
+                hasattr(subclass, 'add_user') and
+                callable(subclass.add_user) and
+                hasattr(subclass, 'remove_user') and
+                callable(subclass.remove_user) and
+                hasattr(subclass, 'edit_user') and
+                callable(subclass.edit_user) or
+                NotImplemented)
+
+
+    @abc.abstractmethod
+    def get_by_id(self, user_id: int):
+        pass
+
+
+    @abc.abstractmethod
+    def get_all(self):
+        pass
+
+
+    @abc.abstractmethod
+    def add(self, user: User):
+        pass
+
+
+    @abc.abstractmethod
+    def remove(self, user_id):
+        pass
+
+
+    @abc.abstractmethod
+    def edit(self, user: User):
+        pass
+
+
