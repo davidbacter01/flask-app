333b0eb2a5539b0c1bd0051e0cd1e666181dfe08
davidbacter01 <davidbacter01@gmail.com>
2020-10-16 03:41:39 +0300

    swap to use of database instead of posts

--- FlaskApp/views/setup_views.py
+++ FlaskApp/views/setup_views.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, redirect, render_template, request
 from services.services import Services
 from models.database_settings import DatabaseSettings
+from setup.database import Database
+from setup.dbconfig import DbConfig
 
 
 
@@ -8,15 +10,15 @@ setup_views_blueprint = Blueprint('setup_views', __name__)
 
 @setup_views_blueprint.route("/setup", methods=['GET', 'POST'])
 def db_setup():
-    posts = Services.get_service(Services.posts)
+    database = Database(DbConfig('postgres'))
     if request.method == 'GET':
         if not Services.TESTING:
-            posts.database.update()
+            database.update()
             return redirect('/index')
         return render_template("db_setup.html")
 
     form_data = request.form
     setup = DatabaseSettings(form_data['dbname'], form_data['user'], form_data['password'])
     Services.get_service(Services.config).save_dbsetup(setup)
-    posts.database.setup()
+    database.setup()
     return redirect('/index')
